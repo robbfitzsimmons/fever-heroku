d7f3499bcef115f46be45c6bbd7eaefda4825acb
Robb Fitzsimmons <robb@proximate.com>
2013-03-15 16:44:41 -0400

    Finding db info

---
+++ find_info.php
@@ -0,0 +1,13 @@
+<?php echo "Hello world!\n"; ?>
+<?php
+$services_json = json_decode(getenv("VCAP_SERVICES"),true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+$username = $mysql_config["username"];
+$password = $mysql_config["password"];
+$hostname = $mysql_config["hostname"];
+$port = $mysql_config["port"];
+$db = $mysql_config["name"];
+$link = mysql_connect("$hostname:$port", $username, $password);
+$db_selected = mysql_select_db($db, $link);
+
+?>
